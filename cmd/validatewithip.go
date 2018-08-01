05e2d68eb4c4edfa5c8391ae6381e314aa56a44e
Henrique Lopes <riquellopes@gmail.com>
2018-07-31 22:14:32 -0300

    implementing the command validate with ip

--- cmd/validatewithip.go
+++ cmd/validatewithip.go
@@ -1,6 +1,7 @@
 package cmd
 
 import (
+	"encoding/json"
 	"fmt"
 
 	"github.com/spf13/cobra"
@@ -12,9 +13,18 @@ var validatewithip = &cobra.Command{
 	Long: ` Get information about the email address, such as
         bounce status, First Name, Last Name, using an IP Address
         for live validations.`,
-	Args: cobra.MinimumNArgs(1),
+	Args: cobra.RangeArgs(1, 2),
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("bye called")
+		ip := ""
+		if len(args) == 2 {
+			ip = args[1]
+		}
+
+		validate, _ := Zero.ValidateWithip(args[0], ip)
+
+		response, _ := json.MarshalIndent(validate, "", " ")
+
+		fmt.Println(string(response))
 	},
 }
 
